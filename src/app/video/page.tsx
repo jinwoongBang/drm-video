3001cc8f08153743e66d1fd61a0d4d9d8e1e1ad9
jinwoongBang <dlopo123@gmail.com>
2024-10-16 00:26:53 +0900

    feature : 레이아웃 컴포넌트 추가

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,5 +1,9 @@
 function Page() {
-  return <div>Video</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      Video
+    </div>
+  );
 }
 
 export default Page;
