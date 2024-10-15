927aa60534b25e85ff26949e801f63410b89e3c0
jinwoongBang <dlopo123@gmail.com>
2024-10-16 00:25:03 +0900

    feature : 커스텀 에러 구현체 추가

---
+++ src/constants/error.ts
@@ -0,0 +1,25 @@
+class SpoonError implements Error {
+  name: string;
+  message: string;
+
+  constructor(message: string) {
+    this.name = this.constructor.name;
+    this.message = message;
+  }
+}
+
+export class NetworkError extends SpoonError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+export class TimeoutError extends SpoonError {
+  constructor(message: string) {
+    super(message);
+  }
+}
+export class InternalServerError extends SpoonError {
+  constructor(message: string) {
+    super(message);
+  }
+}
