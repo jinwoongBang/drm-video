f1498920167f894375eb57206c5b1177e994a179
jinwoongBang <dlopo123@gmail.com>
2024-10-17 00:15:18 +0900

    feature : 에피소드 인덱스 관련 스토어 추가

--- src/store/video/index.ts
+++ src/store/video/index.ts
@@ -4,3 +4,8 @@ export const selectedProgramIdState = atom<string>({
   key: "selectedProgramIdState",
   default: "",
 });
+
+export const selectedEpisodeIndexState = atom<number>({
+  key: "selectedEpisodeIndexState",
+  default: 0,
+});
