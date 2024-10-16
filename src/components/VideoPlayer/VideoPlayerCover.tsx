5bc862912ad3cf342f4ece23bb3e554b99377528
jinwoongBang <dlopo123@gmail.com>
2024-10-16 22:59:38 +0900

    refactor : 비디오 플레이어 커버의 아이콘 버튼 변경 및 스타일링 수정

--- src/components/VideoPlayer/VideoPlayerCover.tsx
+++ src/components/VideoPlayer/VideoPlayerCover.tsx
@@ -1,19 +1,29 @@
 "use client";
+
+import {
+  HeartIcon,
+  ShareIcon,
+  ListBulletIcon,
+  BookmarkIcon,
+  ArrowLeftIcon,
+} from "@heroicons/react/16/solid";
+import IconButton from "@/components/button";
+
 function VideoPlayerCover() {
   return (
-    <section className="absolute top-0 left-0 flex flex-col justify-between w-full h-full">
+    <section className="absolute top-0 left-0 p-4 flex flex-col justify-between w-full h-full">
       <header className="flex justify-between items-center">
-        <div>
-          <button>뒤로가기</button>
+        <div className="flex items-center gap-2">
+          <IconButton Icon={ArrowLeftIcon} />
           <span>내 남친은 찐따입니다.</span>
         </div>
         <div>0/60</div>
       </header>
       <aside className="absolute right-[10px] bottom-[72px] w-[42px] h-[296px] flex flex-col place-items-center justify-between z-[1100]">
-        <button>좋아요</button>
-        <button>공유</button>
-        <button>댓글</button>
-        <button>저장</button>
+        <IconButton Icon={HeartIcon} text="22.5K" />
+        <IconButton Icon={BookmarkIcon} text="찜" />
+        <IconButton Icon={ListBulletIcon} text="목록" />
+        <IconButton Icon={ShareIcon} text="공유" />
       </aside>
       <section>
         <h1>비디오 컨트롤러</h1>
