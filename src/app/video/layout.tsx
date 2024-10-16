f533cab4975b971b6eb04bcc47bdbce07abc5d9c
jinwoongBang <dlopo123@gmail.com>
2024-10-16 21:38:10 +0900

    feature : 리액트 쿼리 하이드레이션 추가 및 swiper 추가

--- src/app/video/layout.tsx
+++ src/app/video/layout.tsx
@@ -1,7 +1,22 @@
-export default function VideoLayout({
+import getQueryClient, { getProgram } from "@/query";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default async function VideoLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>layout {children}</main>;
+  const queryClient = getQueryClient();
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <main>{children}</main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </HydrationBoundary>
+  );
 }
--- src/context/ReactQueryProvider.tsx
+++ src/context/ReactQueryProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
 const ReactQueryProviders = ({ children }: { children: React.ReactNode }) => {
@@ -9,7 +9,7 @@ const ReactQueryProviders = ({ children }: { children: React.ReactNode }) => {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 1 * 1000,
+            staleTime: 60 * 1000,
           },
         },
       })
