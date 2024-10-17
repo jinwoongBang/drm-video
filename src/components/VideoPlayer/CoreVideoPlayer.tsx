36e7ae1096345adb5373ee4a22d555b0622cacf1
jinwoongBang <dlopo123@gmail.com>
2024-10-17 22:34:29 +0900

    feature : shaka player 적용

--- src/components/VideoPlayer/CoreVideoPlayer.tsx
+++ src/components/VideoPlayer/CoreVideoPlayer.tsx
@@ -1,45 +1,62 @@
+"use client";
+
 import { VideoData } from "@/components/shorts/ShortsSwiper";
+import { ShakaPlayerController } from "@/libs/ShakaPlayerController";
 import { isVideoPlayingState } from "@/store/video";
-import { useEffect, useRef } from "react";
+import { VideoPlayInfo } from "@/types/response";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 
 interface VideoPlayerProps {
-  video: VideoData;
+  videoInfo?: VideoPlayInfo | null;
   isActive: boolean;
 }
 
-function VideoPlayer({ video, isActive }: VideoPlayerProps) {
+function VideoPlayer({ videoInfo, isActive }: VideoPlayerProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const shakaPlayerControllerRef = useRef(new ShakaPlayerController());
+
+  const [isInit, setIsInit] = useState(false);
+
   const [isVideoPlaying, setIsVideoPlaying] =
     useRecoilState(isVideoPlayingState);
 
   useEffect(() => {
-    // console.log("video", video);
-    // console.log("isActive", isActive);
-  }, [video, isActive]);
-
-  useEffect(() => {
     if (isActive) {
-      if (isVideoPlaying) {
-        videoRef.current?.play();
-      } else {
-        videoRef.current?.pause();
-      }
+      shakaPlayerControllerRef.current
+        .initApp(videoRef.current as HTMLVideoElement)
+        .then(() => {
+          setIsInit(true);
+        })
+        .then(() => {
+          shakaPlayerControllerRef.current.loadVideo(videoInfo?.url as string);
+        });
+    } else {
+      shakaPlayerControllerRef.current.detach();
     }
-  }, [isVideoPlaying, isActive]);
+
+    return () => {
+      shakaPlayerControllerRef.current.detach();
+    };
+  }, [videoInfo, isActive]);
 
   return (
     <section className="relative flex justify-center align-middle w-screen h-screen">
-      <video
-        ref={videoRef}
-        src={video.videoUrl}
-        className="max-w-[572px] w-full"
-        //   controls
-        loop
-        playsInline
-        autoPlay={false}
-        muted={true}
-      />
+      {isActive && (
+        <video
+          ref={videoRef}
+          // src={videoInfo?.url}
+          className="max-w-[572px] w-full"
+          //   controls
+          poster={
+            "https://content.vigloo.com/media/kr010/p01/s01/e001/thumbnails/3dfd04bd9b6946fa99bf54c28c93d628_thumbnail.0000000.jpg"
+          }
+          loop
+          playsInline
+          autoPlay={true}
+          muted={true}
+        />
+      )}
     </section>
   );
 }
