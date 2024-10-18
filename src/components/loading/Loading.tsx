fd5268451452e0fda65d2eb3cae71e6e46595cc7
jinwoongBang <dlopo123@gmail.com>
2024-10-18 21:14:45 +0900

    refactor : 로딩 컴포넌트 z-index 수정

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className="h-screen w-full">
+    <div className="relative h-screen w-full z-10">
       <div className="flex items-center justify-center h-full">
         <div className="w-10 h-10 border-t-2 border-b-2 border-white-900 rounded-full animate-spin"></div>
       </div>
