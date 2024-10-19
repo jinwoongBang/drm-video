fe97c6c836117983dadaed0fcd654313400dab55
jinwoongBang <dlopo123@gmail.com>
2024-10-19 21:14:08 +0900

    feature : Shaka Error 타입 및 객체 구현

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -1,25 +1,62 @@
 export class SpoonError implements Error {
+  code: string;
   name: string;
   message: string;
 
-  constructor(message: string) {
+  constructor({ code, message }: { code: string; message: string }) {
     this.name = this.constructor.name;
+    this.code = code;
     this.message = message;
   }
 }
 
 export class NetworkError extends SpoonError {
   constructor(message: string) {
-    super(message);
+    super({ code: "NETWORK_ERROR", message });
   }
 }
 export class TimeoutError extends SpoonError {
   constructor(message: string) {
-    super(message);
+    super({ code: "TIMEOUT_ERROR", message });
   }
 }
 export class InternalServerError extends SpoonError {
   constructor(message: string) {
-    super(message);
+    super({ code: "INTERNAL_SERVER_ERROR", message });
+  }
+}
+
+export class UnknownError extends SpoonError {
+  constructor(message: string) {
+    super({ code: "UNKNOWN_ERROR", message });
+  }
+}
+
+export class ShakaError
+  extends SpoonError
+  implements Omit<shaka.util.Error, "noStructuralTyping_shaka_util_Error">
+{
+  code: any;
+  name: string;
+  message: string;
+  data: any;
+  handled: any;
+
+  category: any;
+  severity: any;
+
+  stack?: string | undefined;
+  cause?: unknown;
+
+  constructor(error: shaka.util.Error) {
+    super({ code: error.code, message: error.message });
+    console.error(error);
+
+    this.name = error.name;
+    this.message = error.message;
+    this.category = error.category;
+    this.severity = error.severity;
+    this.stack = error.stack;
+    this.cause = error.cause;
   }
 }
