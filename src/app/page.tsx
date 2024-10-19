2aec3cfab29e37855213b43156265d58e0b7765e
jinwoongBang <dlopo123@gmail.com>
2024-10-19 21:24:40 +0900

    refactor : 메인 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center gap-9 h-screen">
+    <div className="flex flex-col items-center justify-center gap-9 h-screen font-bold">
       <h1>Spoon Video List</h1>
 
-      <span className="flex items-center gap-2">
+      <span className="flex items-center gap-2 border border-white p-2 rounded-lg">
         <ArrowRightCircleIcon className="w-4 h-4" />
-        <Link href="/video/15000001">Video 페이지로 이동</Link>
+        <Link href="/video/15000001">클릭 시 Video 페이지로 이동</Link>
       </span>
     </div>
   );
