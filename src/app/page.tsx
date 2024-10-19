bc16c5a469e2d7b0e6c2888d4313ca393e7a5640
jinwoongBang <dlopo123@gmail.com>
2024-10-19 21:23:23 +0900

    feature : 간이 프로그램 목록 메인 페이지에 쇼츠 페이지로 이동되는 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { ArrowRightCircleIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col items-center justify-center gap-9 h-screen">
+      <h1>Spoon Video List</h1>
+
+      <span className="flex items-center gap-2">
+        <ArrowRightCircleIcon className="w-4 h-4" />
+        <Link href="/video/15000001">Video 페이지로 이동</Link>
+      </span>
     </div>
   );
 }
