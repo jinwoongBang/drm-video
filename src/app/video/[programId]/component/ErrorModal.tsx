acd156ea84c9efa993ec952c8d51a2b4998cda9a
jinwoongBang <dlopo123@gmail.com>
2024-10-19 21:16:39 +0900

    refactor : Shaka Error 객체 여부 확인 후 에러 크리티컬 코드, 카테고리, 에러 코드 노출되도록 수정

--- src/app/video/[programId]/component/ErrorModal.tsx
+++ src/app/video/[programId]/component/ErrorModal.tsx
@@ -5,21 +5,37 @@ import { useRecoilState } from "recoil";
 
 import Modal from "@/components/modal";
 import { errorState } from "@/store/error";
+import { ShakaError } from "@/constants/error";
+import { useRouter } from "next/navigation";
+// import { useRouter } from "next/router";
 
 function ErrorModal() {
+  const router = useRouter();
+
   const [error, setError] = useRecoilState(errorState);
 
   const isOpen = useMemo(() => !!error, [error]);
 
+  const errorMessage = useMemo(() => {
+    if (!error) return "";
+
+    if (error instanceof ShakaError) {
+      return `[${error.severity} - ${error.category} - ${error.code}] 오류가 발생했습니다. 다시 시도해 주세요!`;
+    }
+
+    return `[${error.code}] ${error.message}`;
+  }, [error]);
+
   const handleClose = () => {
     setError(null);
+    router.push(`/`);
   };
 
   return (
-    <Modal isOpen={true} onClose={handleClose}>
+    <Modal isOpen={isOpen} onClose={handleClose}>
       <div className="bg-black text-white p-6 rounded-lg w-4/6">
         {/* <h2 className="text-xl font-bold mb-4">오류 발생</h2> */}
-        <p className="mb-6">{error?.message}</p>
+        <p className="mb-6">{errorMessage}</p>
         <button
           onClick={handleClose}
           className="w-full bg-white text-black font-bold py-2 px-4 rounded hover:bg-gray-200 transition-colors"
