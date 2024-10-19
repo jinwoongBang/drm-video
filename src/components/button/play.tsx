1ee38edcd2d7068470e2f3be6317991cde256093
jinwoongBang <dlopo123@gmail.com>
2024-10-19 15:24:30 +0900

    feature : 재생 & 일시정지 버튼 컴포넌트 추가

---
+++ src/components/button/play.tsx
@@ -0,0 +1,38 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+
+import { isVideoPlayingState } from "@/store/video";
+import clsx from "clsx";
+
+interface PlayButtonProps {
+  playIcon: () => React.ReactNode;
+  pauseIcon: () => React.ReactNode;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+function PlayButton({
+  playIcon,
+  pauseIcon,
+  className,
+  onClick,
+}: PlayButtonProps) {
+  const [isVideoPlaying, setIsVideoPlaying] =
+    useRecoilState(isVideoPlayingState);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onClick && onClick(event);
+    event.stopPropagation();
+    setIsVideoPlaying((state) => !state);
+  };
+
+  return (
+    <button
+      className={clsx(className, "flex justify-center items-center")}
+      onClick={handleClick}
+    >
+      {isVideoPlaying ? playIcon() : pauseIcon()}
+    </button>
+  );
+}
+
+export default PlayButton;
